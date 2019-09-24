9e51a287c5ddbb623a4de296162e18e1d16ce3ba
PetejStone <petejstoney@gmail.com>
2019-09-24 14:31:54 -0700

    added more rooms

--- util/create_world.py
+++ util/create_world.py
@@ -31,13 +31,23 @@ continue south or go back from where you came""")
 
 r_stunnel2= Room(title="South Tunnel 2", description="""You've gone further in the south tunnel. You can still continue south""")
 
-r_stunnel3= Room(title="South Tunnel 2", description="""You've gone as far as you can go. Looks like a dead end. Better go back.""")
+r_stunnel3= Room(title="South Tunnel 3", description="""You've gone as far as you can go. Looks like a dead end. Better go back.""")
+
+r_etunnel2= Room(title="East Tunnel 2", description="""You've continues east. There is another tunnel going south again, or it continues east""")
+
+r_stunnel4= Room(title="SouthTunnel 4", description="""You've gone south. The tunnel continues in the same direction""")
+
+r_stunnel5= Room(title="SouthTunnel 5", description="""You have reached deeper in the tunnel. You think you hear something ahead. You can continue south to investigate, or go back""")
+
+r_stunnel6= Room(title="SouthTunnel 6", description="""It was just the rocks settling in the cave. Another dead end. You better go back""")
 
 r_outside.save()
 r_foyer.save()
 r_overlook.save()
 r_narrow.save()
 r_treasure.save()
+r_etunnel.save()
+r_etunnel2.save()
 
 # Link rooms together
 r_outside.connectRooms(r_foyer, "n")
@@ -54,6 +64,21 @@ r_treasure.connectRooms(r_narrow, "s")
 
 r_treasure.connectRooms(r_overlook, "w")
 r_overlook.connectRooms(r_treasure, "e")
+
+r_treasure.connectRooms(r_etunnel, "e")
+r_etunnel.connectRooms(r_treasure, "w")
+
+r_etunnel.connectRooms(r_stunnel, "s")
+r_stunnel.connectRooms(r_etunnel, "n")
+
+r_stunnel.connectRooms(r_stunnel2, "s")
+r_stunnel2.connectRooms(r_stunnel, "n")
+
+r_stunnel2.connectRooms(r_stunnel3, "s")
+r_stunnel3.connectRooms(r_stunnel2, "n")
+
+r_etunnel.connectRooms(r_etunnel2, "e")
+r_etunnel2.connectRooms(r_etunnel, "w")
 
 
 
