e11a6c53ec22889b090f35c5c3c1eafc4c31949e
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-24 10:20:35 -0700

    Update settings.py

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -18,9 +18,6 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
