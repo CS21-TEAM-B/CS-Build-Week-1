11bf9e41e66580cef00948d46cf8ee479f467af4
PetejStone <petejstoney@gmail.com>
2019-09-26 10:24:28 -0700

    added directions

--- adventure/api.py
+++ adventure/api.py
@@ -16,7 +16,7 @@ import json
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Room
-        fields = ("title", "description")
+        fields = ("id", "title", "description", "n_to", "s_to", "e_to", "w_to")
 
 class RoomViewSet(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
